fd1ade49fe798dd2c469f54c2472372a9b3b20f0
José Manuel Alfonso Espino <josemaae96ah@gmail.com>
2021-02-17 23:41:03 +0100

    Siguiendo con los ejercicios, paradita tecnica

---
+++ src/Ejercicios/Tres.java
@@ -0,0 +1,22 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class Tres {
+	//Une dos cadenas i.p.t. usando el método concat()
+	
+public static void main(String[] args) {
+		
+		Scanner leer = new Scanner(System.in);
+		String cadenaUno;
+		String cadenaDos;
+		
+		System.out.println("Introduce una cadena");
+		cadenaUno=leer.nextLine();
+		System.out.println("Introduce otra cadena");
+		cadenaDos=leer.nextLine();
+		
+		System.out.println(cadenaUno+" "+cadenaDos);
+		System.out.println(cadenaUno.concat(" "+cadenaDos));
+}
+}
